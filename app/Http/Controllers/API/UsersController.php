879f9680073f94422b0c25859871a95525782e99
Fares <88165859+faresll@users.noreply.github.com>
2022-10-12 15:08:14 +0200

    create index function to return json users

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\API\Users;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    //use GeneralTrait;
+    public function index()
+    {
+        $Users = Users::get();
 
+        return response() ->json($Users);
+    }
 }
