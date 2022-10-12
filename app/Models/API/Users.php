9ab71dd50dd940573d19ee4a678bac773430e5cb
Fares <88165859+faresll@users.noreply.github.com>
2022-10-12 15:07:25 +0200

    rename user model

--- app/Models/API/Users.php
+++ app/Models/API/Users.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Users extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
         'user_add',
+        'user_phone'
     ];
 
     /**
@@ -30,7 +31,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'email_verified_at',
         'remember_token',
     ];
 
