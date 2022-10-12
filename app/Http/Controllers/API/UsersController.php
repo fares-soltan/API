e0a52c8b90cb81bcfbc291cb7c793b96a6684e3e
Fares <88165859+faresll@users.noreply.github.com>
2022-10-13 01:53:19 +0200

    create login function to create jwt token

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -7,10 +7,54 @@ use App\Models\API\Users;
 use App\Traits\GeneralTrait;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     use GeneralTrait;
+
+    public function login(Request $request)
+    {
+
+        try {
+            $rules = [
+                "email" => "required",
+                "password" => "required"
+
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                $code = $this->returnCodeAccordingToInput($validator);
+                return $this->returnValidationError($code, $validator);
+            }
+
+            //login
+
+            $credentials = $request->only(['email', 'password']);
+
+            $token = Auth::guard('user-api')->attempt($credentials);  //generate token
+
+            if (!$token)
+                return $this->returnError('E001', 'بيانات الدخول غير صحيحة');
+
+            $user = Auth::guard('user-api')->user();
+            $user ->api_token = $token;
+            //return token
+            return $this->returnData('user', $user);  //return json response
+
+        } catch (\Exception $ex) {
+            return $this->returnError($ex->getCode(), $ex->getMessage());
+        }
+    }
+
+
+
+
+
     public function index(){
         $Users = Users::get();
 
@@ -45,4 +89,6 @@ class UsersController extends Controller
         Users::where('id','=',$request-> id) -> update(['password' => $request-> password]);
         return $this->returnSuccessMessage('changed Password...!');
     }
+
+
 }
