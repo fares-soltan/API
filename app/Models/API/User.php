6a2d3948242fdb85585704b52669d7cf116cf452
Fares <88165859+faresll@users.noreply.github.com>
2022-10-12 14:49:53 +0200

    .

--- app/Models/API/User.php
+++ app/Models/API/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\API;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_add',
     ];
 
     /**
